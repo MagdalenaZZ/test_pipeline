e40557f5d6a390093e92097b2bb4525758e3235a
magdalenazz <magza477@marvin-new.cgu10.igp.uu.se>
2023-09-25 15:06:50 +0200

    Init

---
+++ docs/softwares.md
@@ -0,0 +1,1 @@
+# Softwares used in the test_pipeline module
---
+++ workflow/scripts/dummy.py
@@ -0,0 +1,11 @@
+# vim: syntax=python tabstop=4 expandtab
+# coding: utf-8
+
+__author__ = "Magdalena Zarowiecki"
+__copyright__ = "Copyright 2023, Magdalena Zarowiecki"
+__email__ = "magdalena.z@scilifelab.uu.se"
+__license__ = "GPL-3"
+
+
+def dummy():
+    return 1
